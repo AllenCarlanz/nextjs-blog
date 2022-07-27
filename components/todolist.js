04a2c6b902c1269179ee0bfa2a25b9ef79678f7d
Allen Carlanz Ramos <allencarlanzramos@Allens-MacBook-Pro.local>
2022-07-27 15:51:41 +0800

    commit

--- components/todolist.js
+++ components/todolist.js
@@ -5,7 +5,7 @@ import TodoListItems from './todolist-items';
 export default function TodoList({ todoListData }) {
     return (
         <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-            <h2 className={utilStyles.headingLg}>To-Do List</h2>
+            <h2 className={utilStyles.headingLg}>To-Do List (API source tutorial)</h2>
             <ul className={utilStyles.list}>
                 {todoListData.slice(0, 5).map(({ id, title, completed }) =>
                 (
